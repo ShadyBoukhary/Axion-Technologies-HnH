1eebacf198fa4aea3c4ec2cf9bd3f66a459a76aa
Shady Boukhary <sb199898.sb@gmail.com>
2019-01-27 00:16:31 -0600

    GUI fix

--- lib/app/home/home_view.dart
+++ lib/app/home/home_view.dart
@@ -18,12 +18,30 @@ class HomePageView extends State<HomePage> implements View {
 
   void callHandler(Function fn) {
     setState(() {
-     fn(); 
+      fn();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var columnChildren = <Widget>[
+      Text(
+        'You have pushed the button this many times:',
+      ),
+      Text(
+        "${_controller.counter}",
+        style: Theme.of(context).textTheme.display1,
+      ),
+      RaisedButton(
+        child: Text("Get User"),
+        onPressed: () {
+          callHandler(_controller.getUser);
+        },
+      ),
+    ];
+    if (!_controller.hidden) {
+      columnChildren.add(Text('Name: ${_controller.currentUser?.firstName} ${_controller.currentUser?.lastName}'));
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -35,28 +53,14 @@ class HomePageView extends State<HomePage> implements View {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              "${_controller.counter}",
-              style: Theme.of(context).textTheme.display1,
-            ),
-            RaisedButton(
-              child: Text("Get User"),
-              onPressed: () { callHandler(_controller.getUser); },
-            ),
-            Text(
-              'Name: ${_controller.currentUser?.firstName} ${_controller.currentUser?.lastName}'
-            )
-          ],
+          children: columnChildren
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () { callHandler(_controller.incrementCounter); },
+        onPressed: () {
+          callHandler(_controller.incrementCounter);
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
