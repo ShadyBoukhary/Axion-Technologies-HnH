0775ff52608d8242676e18fbe2fb4184b3d5febb
Shady Boukhary <sb199898.sb@gmail.com>
2019-02-07 13:01:15 -0600

    updated User class constructor from a map

--- lib/domain/repositories/authentication_repository.dart
+++ lib/domain/repositories/authentication_repository.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-
+import 'package:hnh/domain/entities/user.dart';
 /// Abstract class that defines the expected behavior of an [AuthenticationRepository]
 /// A repository tasked with user authentication and registration
 /// To be implemented in the Data layer
@@ -11,8 +11,11 @@ abstract class AuthenticationRepository {
   /// Authenticates a user using his [username] and [password]
   void authenticate({@required String username, @required String password});
 
-  /// Returns a `bool` whether the current user is authenticated
-  bool isAuthenticated();
+  /// Returns a `Future<bool>` whether the current user is authenticated
+  Future<bool> isAuthenticated();
+
+  // Returns the current authenticated `User` wrapped in a `Future`
+  Future<User> getCurrentUser();
 
   /// Resets the password of a user
   void resetPassword();
