be8de7a10da09041e2d967f35e8f9ff272605ff1
Shady Boukhary <sb199898.sb@gmail.com>
2019-02-08 23:48:16 -0600

    implemented login presenter and login listeners in the controller: needs futher changes, fixed logging listener in main.dart

--- lib/domain/usecases/auth/login_usecase.dart
+++ lib/domain/usecases/auth/login_usecase.dart
@@ -2,6 +2,7 @@ import 'package:hnh/domain/repositories/authentication_repository.dart';
 import 'package:hnh/domain/usecases/usecase.dart';
 import 'package:hnh/domain/entities/user.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:logging/logging.dart';
 import 'dart:async';
 
 /// A `UseCase` for logging in a `User` into the application
@@ -9,18 +10,18 @@ class LoginUseCase extends CompletableUseCase<LoginUseCaseParams> {
 
   // Members
   AuthenticationRepository _authenticationRepository;
-
+  Logger _logger;
   // Constructors
-  LoginUseCase(this._authenticationRepository);
+  LoginUseCase(this._authenticationRepository) { _logger = Logger('LoginUseCase'); }
 
   @override
   Future<Observable<User>> buildUseCaseObservable(LoginUseCaseParams params) async {
     final StreamController<User> controller = StreamController();
     try {
-      await _authenticationRepository.authenticate(username: params._username, password: params._password);
+      await _authenticationRepository.authenticate(email: params._email, password: params._password);
       controller.close();
     } catch (e) {
-      print(e);
+      _logger.shout('Could not login the user.', e);
       controller.addError(e);
     }
     return Observable(controller.stream);
@@ -29,8 +30,8 @@ class LoginUseCase extends CompletableUseCase<LoginUseCaseParams> {
 
 /// The parameters required for the [LoginUseCase]
 class LoginUseCaseParams {
-  String _username;
+  String _email;
   String _password;
 
-  LoginUseCaseParams(this._username, this._password);
+  LoginUseCaseParams(this._email, this._password);
 }
