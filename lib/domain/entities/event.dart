62d5c72f05ddd7b33a2935e715d0ad7bc3124e84
Shady Boukhary <sb199898.sb@gmail.com>
2019-02-17 11:17:14 +0700

    added id to Event and EventRegistration, created uuid generator function in utils class

--- lib/domain/entities/event.dart
+++ lib/domain/entities/event.dart
@@ -9,24 +9,27 @@ class Event {
   String _name;
   String _description;
   Location _location; // also contains the start time
+  String _id;
   List<Coordinates> _route;
 
   // Getters
   String get name => _name;
   String get description => _description;
   Location get location => _location;
+  String get id => _id;
   List<Coordinates> get route => _route;
 
   // Constructors
 
   // Default
-  Event(this._name, this._description, this._location, [this._route]);
+  Event(this._name, this._description, this._location, this._id, [this._route]);
 
   /// From an [Event]
   Event.fromEvent(Event event) {
     _name = event._name;
     _description = event._description;
     _location = Location.fromLocation(event._location);
+    _id = event._id;
     _route = List<Coordinates>()..addAll(event._route);
   }
 
@@ -35,6 +38,7 @@ class Event {
     _name = map['name'];
     _description = map['description'];
     _location = Location.fromJson(map['location'] );
+    _id = map['id'];
     var coordsIt = (map['route'] as List<Map<String, dynamic>>).map((map) => Coordinates.fromJson(map));
     _route = List<Coordinates>.from(coordsIt);
   }
@@ -44,6 +48,7 @@ class Event {
     'name': _name,
     'description': _description,
     'location': _location.toJson(),
+    'id': _id,
     'route': jsonDecode(jsonEncode(_route))
   };
 
