2031ccfe3a30b9efdf112f3544cfbe3a0c7e021c
Shady Boukhary <sb199898.sb@gmail.com>
2019-02-26 21:40:18 -0600

    implemented EventRepository

--- lib/data/utils/constants.dart
+++ lib/data/utils/constants.dart
@@ -2,7 +2,7 @@ class Constants {
     static const String baseUrl = 'http://45.32.197.143:8000';
     static const String usersRoute = '$baseUrl/users';
     static const String loginRoute = '$baseUrl/login';
-
+    static const String eventsRoute = '$baseUrl/events';
 
     static const String tokenKey = 'authentication_token';
     static const String userKey = 'user_key';
