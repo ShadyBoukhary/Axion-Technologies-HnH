dc0b3a65a09e4d7d12b557b57d6a77629ed187fd
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-10 03:26:50 -0500

    created domain SponsorRepository

--- lib/domain/repositories/user_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hnh/domain/entities/user.dart';
-import 'dart:async';
-
-abstract class UserRepository {
-  void saveUser(User user);
-  Future<User> getUser(String uid);
-  Future<List<User>> getAllUsers();
-}
