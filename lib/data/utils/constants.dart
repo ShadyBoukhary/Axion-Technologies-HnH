63a83e7fb3413fed0d5da223520e9c845ca68635
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-10 03:28:55 -0500

    created http helper

--- lib/data/utils/constants.dart
+++ lib/data/utils/constants.dart
@@ -1,13 +1,20 @@
 class Constants {
+
+    // APIs
     static const String baseUrl = 'http://$baseUrlNoPrefix';
-    static const String baseUrlNoPrefix = '45.32.197.143:8000';
     static const String usersRoute = '$baseUrl/users';
     static const String loginRoute = '$baseUrl/login';
     static const String eventsRoute = '$baseUrl/events';
     static const String eventRegistrationsRoute = '$baseUrl/eventRegistrations';
     static const String allHHHRoute = '$baseUrl/HHHs';
     static const String currentHHHRoute = '$baseUrl/currentHHH';
+
+    // APIs no prefix
+    static const String baseUrlNoPrefix = '45.32.197.143:8000';
     static const String eventsPathOnly = '/events';
+    static const String sponsorsPathOnly = '/sponsors';
+
+    // Local Storage
     static const String tokenKey = 'authentication_token';
     static const String userKey = 'user_key';
     static const String isAuthenticatedKey = 'isUserAuthenticated';
