b0318e97e0d1489a7dc6f5328440b644780e8b47
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-10 03:31:05 -0500

    backend: created SPONSORS array and updated HHH data

--- backend/app/data/hhh.ts
+++ backend/app/data/hhh.ts
@@ -1,5 +1,6 @@
 import { HHH } from "../interfaces/non_modals/hhh";
 import { EVENTS } from './events';
+import { SPONSORS } from "./sponsors";
 
 export const HHHs: HHH[] = [
     {
@@ -7,15 +8,15 @@ export const HHHs: HHH[] = [
         description: 'A lot of things are going on during the HHH weekend but there is no way to describe the electrifying experience of the START. Riders begin to assemble as early as 4AM. They are joined by 10,000+ other riders who have trained to complete their chosen distances. All that pent up human energy is unleashed after the American National Anthem, Air Force Fly Over and Cannon Blast. If you are going to ride 100 miles, the best place to do it is at the Hotter’N Hell Hundred!',
         mailingAddress: 'Hotter\'N Hell Hundred, PO Box 2099, Wichita Falls, TX 76307',
         timestamp: '1566622800',
-        sponsors: ['djsfhsjdkhfkjs243'],
-        events: EVENTS.map((event) => event._id),
+        sponsors: SPONSORS.filter((sponsor) => sponsor.year === '2019').map((sponsor) => sponsor.name),
+        events:  EVENTS.map((event) => event._id),
     },
     {
         id: '2018',
         description: 'A lot of things are going on during the HHH weekend but there is no way to describe the electrifying experience of the START. Riders begin to assemble as early as 4AM. They are joined by 10,000+ other riders who have trained to complete their chosen distances. All that pent up human energy is unleashed after the American National Anthem, Air Force Fly Over and Cannon Blast. If you are going to ride 100 miles, the best place to do it is at the Hotter’N Hell Hundred!',
         mailingAddress: 'Hotter\'N Hell Hundred, PO Box 2099, Wichita Falls, TX 76307',
         timestamp: '1566622800',
-        sponsors: ['djsfhsjdkhfkjs243'],
-        events: ['5c68d750cf2095b99753c693', '5a68d750cf1095b92753c696', '5a68d750cf1095b92753c695'],
+        sponsors: SPONSORS.filter((sponsor) => sponsor.year === '2018').map((sponsor) => sponsor.name),
+        events: EVENTS.map((event) => event._id),
     }
 ];  
