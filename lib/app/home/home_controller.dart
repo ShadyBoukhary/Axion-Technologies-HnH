4639b6a9a40cbca916d2d4223cbec855ff96002f
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-16 22:49:48 -0500

    App: updated home view to work with loading, implemented its controller and presenter

--- lib/app/home/home_controller.dart
+++ lib/app/home/home_controller.dart
@@ -1,37 +1,42 @@
 import 'package:hnh/app/abstract/controller.dart';
-import 'package:hnh/app/home/user_presenter.dart';
+import 'package:hnh/app/home/home_presenter.dart';
 import 'package:hnh/domain/entities/user.dart';
 import 'package:logging/logging.dart';
+import 'package:hnh/domain/entities/hhh.dart';
 
 class HomeController extends Controller {
 
-  UserPresenter _userPresenter;
+  HomePresenter _homePresenter;
   User _currentUser;
+  HHH _currentHHH;
+
+  DateTime get eventTime => _currentHHH?.eventTime;
   User get currentUser => _currentUser;
   Logger logger;
 
-  HomeController() {
+  HomeController(hhhRepository, sponsorRepository) {
+    _homePresenter = HomePresenter(hhhRepository, sponsorRepository);
     initListeners();
+    isLoading = true;
+    retrieveData();
   }
 
   void initListeners() {
-    _userPresenter.getUserOnNext = (User user) {
-      _currentUser = user;
+    _homePresenter.getHHHOnNext = (HHH hhh) {
+      _currentHHH = hhh;
     };
 
-    _userPresenter.getUserOnError = () {
-      logger.shout("ERROR: Could not retrieve user.");
+    _homePresenter.getHHHOnError = (e) {
+      // TODO: show the user the error
+      print(e);
     };
 
-    _userPresenter.getUserOnComplete = () {
-      logger.finest("Completed: User retrieved.");
+    _homePresenter.getHHHOnComplete = () {
+      dismissLoading();
     };
   }
 
-  void getUser() async {
-  }
-
-  void logout(context) {
-    // TODO: Sign out user
+  void retrieveData() {
+    _homePresenter.getCurrentHHH();
   }
 }
