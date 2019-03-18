7d1b773f3935e54d79d4ff2738f6403378b5e1f1
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-17 23:28:58 -0500

    Domain: Created GetUserUseCase

---
+++ lib/domain/usecases/get_current_user_usecase.dart
@@ -0,0 +1,32 @@
+import 'package:hnh/domain/repositories/authentication_repository.dart';
+import 'package:hnh/domain/usecases/usecase.dart';
+import 'package:hnh/domain/entities/user.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:async';
+
+/// Retrieves the currently authenticated [User]
+class GetCurrentUserUseCase extends UseCase<User, void> {
+  AuthenticationRepository _authenticationRepository;
+  GetCurrentUserUseCase(this._authenticationRepository);
+
+  @override
+  Future<Observable<User>> buildUseCaseObservable(void ignore) async {
+    final StreamController<User> controller = StreamController();
+    try {
+
+      User user = await _authenticationRepository.getCurrentUser();
+      controller.add(user);
+      logger.finest('GetCurrentUserUseCase successful.');
+      controller.close();
+    } catch (e) {
+      print(e);
+      logger.severe('GetCurrentUserUseCase unsuccessful.');
+      controller.addError(e);
+    }
+    return Observable(controller.stream);
+  }
+}
+
+
+
+
