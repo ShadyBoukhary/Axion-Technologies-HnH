1b5e4fb1bec5580507502840c22bacdf868f5e87
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-21 00:03:41 -0500

    added splash screen witha animation and auto login. Implemented its controller and presenter

--- lib/app/splash/splash_controller.dart
+++ lib/app/splash/splash_controller.dart
@@ -1,12 +1,47 @@
+import 'package:flutter/animation.dart';
 import 'package:hnh/app/abstract/controller.dart';
 import 'package:hnh/app/splash/splash_presenter.dart';
+import 'package:flutter/material.dart';
 
 class SplashController extends Controller {
   SplashPresenter _splashPresenter;
-
-  SplashController() {
+  
+  SplashController(authRepo) {
+    _splashPresenter = SplashPresenter(authRepo);
     initListeners();
+    getAuthStatus();
   }
 
-  void initListeners() {}
+  /// Initializes [animation] for the view using a given [controller]
+  void initAnimation(AnimationController controller, Animation animation) {
+    animation.addStatusListener((status) {
+      if (!isLoading) {
+        controller.stop(canceled: true);
+      } else if (status == AnimationStatus.completed) {
+        controller.reverse();
+
+      } else if (status == AnimationStatus.dismissed){
+        controller.forward();
+      }
+    });
+    controller.forward();
+  }
+
+  void authStatusOnNext(bool isAuth) {
+    String page = isAuth ? '/home' : '/login';
+    Navigator.of(context).pushReplacementNamed(page);
+  }
+
+  void getAuthStatus() async {
+    isLoading = true;
+    // so the animation can be seen
+    Future.delayed(Duration(seconds: 3), _splashPresenter.getAuthStatus);
+  }
+
+  void initListeners() {
+    _splashPresenter.getAuthStatusOnNext = authStatusOnNext;
+    _splashPresenter.getAuthStatusOnComplete = () => dismissLoading();
+  }
+
+  void dispose() => _splashPresenter.dispose();
 }
