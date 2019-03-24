1b8a693b738f396fd9b7f3e047469029fc889648
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-23 22:50:10 -0500

    created UnRegisterEventUseCase and fixed timestamp double instead of int bug

--- lib/domain/utils/utils.dart
+++ lib/domain/utils/utils.dart
@@ -3,4 +3,5 @@ import 'package:uuid/uuid.dart';
 class Utils {
   static String uuidRandom() => Uuid().v1();
   static String get currentYear => DateTime.now().year.toString();
+  static String get newTimestamp => (DateTime.now().millisecondsSinceEpoch / 1000).floor().toString();
 }
