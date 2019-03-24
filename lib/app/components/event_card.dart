ccd3befe5174f3b4dd3c45a98c4b1aac6650c44c
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-23 19:36:09 -0500

    updated EventCard to display Event info

--- lib/app/components/event_card.dart
+++ lib/app/components/event_card.dart
@@ -1,104 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:hnh/app/utils/constants.dart';
+import 'package:hnh/domain/entities/event.dart';
 
 class EventCard extends StatelessWidget {
-  final String _title;
-  final String _description;
-  final String _location;
-  final String _date;
-  final String _time;
-  final String _photo;
 
-  EventCard(this._title, this._description, this._location, this._date,
-      this._time, this._photo);
+  final Event _event;
+
+  EventCard(this._event);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          width: 300.0,
-          height: MediaQuery.of(context).size.height / 4,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: InkWell(
-              splashColor: Colors.black,
-              onTap: () => {Navigator.pushNamed(context, '/event')},
-              child: Image(
-                image: AssetImage(_photo),
-                fit: BoxFit.cover,
-                alignment: Alignment.center,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 0.0,
-          child: Container(
+    return Padding(
+      padding: const EdgeInsets.only(right: 15.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
             width: 300.0,
-            height: (MediaQuery.of(context).size.height / 4) / 2,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: FractionalOffset.topCenter,
-                end: FractionalOffset.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.0),
-                  Colors.black.withOpacity(0.5),
-                  Colors.black.withOpacity(0.6),
-                  Colors.black.withOpacity(0.7),
-                  Colors.black.withOpacity(0.8),
-                ],
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0),
-              ),
-            ),
-            child: InkWell(
-              splashColor: Colors.black,
-              onTap: () => {Navigator.pushNamed(context, '/event')},
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          _title,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Text(
-                            _description,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w300,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+            height: MediaQuery.of(context).size.height / 4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: InkWell(
+                splashColor: Colors.black,
+                onTap: () => {Navigator.pushNamed(context, '/event')},
+                child: Image.network(
+                  _event.imageUrl,
+                  fit: BoxFit.cover,
+                  alignment: Alignment.center,
                 ),
               ),
             ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 0.0,
+            child: Container(
+              width: 300.0,
+              height: (MediaQuery.of(context).size.height / 4) / 2,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.6),
+                    Colors.black.withOpacity(0.7),
+                    Colors.black.withOpacity(0.8),
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+              ),
+              child: InkWell(
+                splashColor: Colors.black,
+                onTap: () => {Navigator.pushNamed(context, '/event')},
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            _event.name,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Text(
+                              _event.description,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w300,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
