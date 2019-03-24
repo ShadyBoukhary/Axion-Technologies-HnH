675433b1b5ce90c88fa34383a77bf862dd4eb756
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-23 19:35:39 -0500

    added a resumeLoading() method to abstract Controller

--- lib/app/abstract/controller.dart
+++ lib/app/abstract/controller.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class Controller with WidgetsBindingObserver {
-  
   BuildContext context; // needed for navigation
-  Function refresh;  // callback function for refreshing the UI
+  Function refresh; // callback function for refreshing the UI
   bool isLoading; // indicates whether a loading dialog is present
 
   @override
@@ -24,9 +23,27 @@ abstract class Controller with WidgetsBindingObserver {
     }
   }
 
+  /// Dismisses the loading dialog. The parent `View` of this [Controller] should have its body wrapped
+  /// in the ModelHUD and is listening to the [Controller]'s [isLoading]. Otherwise, this method will
+  /// have no impact.
   void dismissLoading() {
-    assert(refresh != null);
+    assert(refresh != null, 'Please set the Controller refresh function inside the View');
     refresh(() => isLoading = false);
+  }
+
+  /// Sets the loading to true. The `View` body should be wrapped in a loader.
+  /// Call on initial page load. For example, if a loader is needed as soon as you open a page.
+  /// Only works when called inside the [Controller] constructor.
+  void startLoading() {
+    isLoading = true;
+  }
+
+  /// Sets the loading to true. The `View` body should be wrapped in a loader.
+  /// Call when a loader is needed after an event (e.g. a button press).
+  /// Does not work if called inside the [Controller] constructor.
+  void resumeLoading() {
+    assert(refresh != null, 'Please set the Controller refresh function inside the View');
+    refresh(() => isLoading = true);
   }
 
   void initListeners();
