30ef36b4ef8e5bd58518b8ef3f9ce02c68d63fe2
Shady Boukhary <sb199898.sb@gmail.com>
2019-04-06 23:27:47 -0500

    updated local place entity to include image url

--- lib/domain/usecases/get_local_places_usecase.dart
+++ lib/domain/usecases/get_local_places_usecase.dart
@@ -1,20 +1,25 @@
 import 'package:hnh/domain/entities/coordinates.dart';
+import 'package:hnh/domain/entities/location.dart';
 import 'package:hnh/domain/repositories/local_places_repository.dart';
+import 'package:hnh/domain/repositories/location_repository.dart';
 import 'package:hnh/domain/usecases/usecase.dart';
 import 'package:hnh/domain/entities/local_place.dart';
 import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 
-class GetLocalPlacesUseCase extends UseCase<List<LocalPlace>, GetLocalPlacesUseCaseParams> {
-  LocalPlacesRepository _localPlacesRepository;
-  GetLocalPlacesUseCase(this._localPlacesRepository);
+class GetLocalPlacesUseCase extends UseCase<List<LocalPlace>, void> {
+  final LocalPlacesRepository _localPlacesRepository;
+  final LocationRepository _locationRepository;
+  GetLocalPlacesUseCase(this._localPlacesRepository, this._locationRepository);
 
   @override
-  Future<Observable<List<LocalPlace>>> buildUseCaseObservable(GetLocalPlacesUseCaseParams params) async {
+  Future<Observable<List<LocalPlace>>> buildUseCaseObservable(_) async {
     final StreamController<List<LocalPlace>> controller = StreamController();
     try {
-      Coordinates coordinates = Coordinates(params._lat, params._lon);
+      Location location = await _locationRepository.getLocation();
+      Coordinates coordinates = Coordinates(location.lat, location.lon);
       List<LocalPlace> places = await _localPlacesRepository.getLocalRestaurants(latlon: coordinates);
+      places.addAll(await _localPlacesRepository.getLocalHotels(latlon: coordinates));
       controller.add(places);
       logger.finest('GetLocalPlacesUseCase successful.');
       controller.close();
@@ -25,12 +30,6 @@ class GetLocalPlacesUseCase extends UseCase<List<LocalPlace>, GetLocalPlacesUseC
     }
     return Observable(controller.stream);
   }
-}
-
-class GetLocalPlacesUseCaseParams {
-  final String _lat;
-  final String _lon;
-  GetLocalPlacesUseCaseParams(this._lat, this._lon);
 }
 
 
