b44cb303cc37483ee9a46cd783da45993abc2bea
Shady Boukhary <sb199898.sb@gmail.com>
2019-04-14 01:40:02 -0500

    Heavily modified location tracking usecase: Now works with updated location repo, retrieves weather every 10 minutes, only updates location when changed

--- lib/domain/usecases/location_track_usecase.dart
+++ lib/domain/usecases/location_track_usecase.dart
@@ -1,33 +1,88 @@
+import 'package:hnh/domain/entities/coordinates.dart';
+import 'package:hnh/domain/entities/weather.dart';
 import 'package:hnh/domain/repositories/location_repository.dart';
+import 'package:hnh/domain/repositories/weather_repository.dart';
 import 'package:hnh/domain/usecases/usecase.dart';
 import 'package:hnh/domain/entities/location.dart';
 import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 
-/// Tracks a user's [Location] every 1 second until the [UseCase] is disposed.
-class LocationTrackUseCase extends UseCase<Location, void> {
-  LocationRepository _locationRepository;
-  LocationTrackUseCase(this._locationRepository) : super();
+/// Tracks a user's [Location] and provides [Weather] every time it changed until the [UseCase] is disposed.
+class LocationTrackUseCase extends UseCase<LocationTrackResponse, void> {
+  // Members
+  final LocationRepository _locationRepository;
+  final WeatherRepository _weatherRepository;
+  DateTime _lastRetrievalTime;
+  Coordinates lastKnownLocation;
+  bool _initialRetrieval;
+
+  LocationTrackUseCase(this._locationRepository, this._weatherRepository)
+      : _lastRetrievalTime = DateTime.now(),
+        _initialRetrieval = true,
+        super();
 
   @override
-  Future<Observable<Location>> buildUseCaseObservable(_) async {
-    // Periodic observable every 1 second
-    return Observable.periodic(Duration(seconds: 1), (second) {
-      // get 1 location observable
-      return Observable.fromFuture(getLocation());
-    }).flatMap((mapper) => mapper)  // flatmap [[loc] [loc] [loc]] => [loc, loc, loc]
-      .where((location) => location != null); // filter out null values if any
+  Future<Observable<LocationTrackResponse>> buildUseCaseObservable(_) async {
+    return _locationRepository
+        // on location change observable
+        .onLocationChanged()
+        // clean out any errors
+        .where((data) => data != null)
+        // convert the location data into a Location entity and get weather
+        .map((data) => convertToResponse(data))
+        // flatmap [[res], [res], [res]] to [res, res, res]
+        .flatMap((mapper) => mapper)
+        // remove duplicates if location has not changed
+        .where((response) =>
+            response != null &&
+            response.location.toCoordinates() != lastKnownLocation)
+        // update last known location
+        .map((response) {
+          lastKnownLocation = response.location.toCoordinates();
+          return response;
+        });
   }
 
-  /// Retrieves current [location] from [_locat]
-  Future<Location> getLocation() async {
-    try {
-      Location location = await _locationRepository.getLocation();
+  /// Converts location data to `LocationTrackResponse` observable
+  Observable<LocationTrackResponse> convertToResponse(data) {
+    // parse location data
+    Location location = Location.withoutTime(data.latitude.toString(), data.longitude.toString(), data.speed);
+    // log info
+    if (location.toCoordinates() != lastKnownLocation) {
       logger.finest('Retrieved location successfully: $location');
-      return location;
-    } catch (e) {
-      logger.warning('Could not update location while tracking.', e);
-      rethrow;
+    } else {
+      logger.info('Did not update location. Identical to last known location');
     }
+    // map every location to `LocationTrackResponse` [[response], [response], [response]]
+    return Observable.fromFuture(getWeather(location));
   }
+
+  /// retrieves new weather data as long as 10 or more minutes have elapsed
+  Future<LocationTrackResponse> getWeather(Location location) async {
+    // find time elapsed between current time and last weather retrieval time
+    DateTime now = DateTime.now();
+    Duration elapsed = now.difference(_lastRetrievalTime);
+
+    // only retrieve new weather data every 10 minutes
+    if (elapsed.inMinutes > 10 || _initialRetrieval) {
+      _initialRetrieval = false;
+      _lastRetrievalTime = DateTime.now();
+      try {
+        Weather weather = await _weatherRepository.getWeather(location.toCoordinates());
+        logger.finest('Retrieved weather successfully');
+        return LocationTrackResponse(location, weather);
+      } catch (e) {
+        logger.shout('Could not retrieve weather.', e);
+        return LocationTrackResponse(location, null);
+      }
+    }
+    logger.info('Did not retrieve new weather data. Time elapsed: ${elapsed.inMinutes}:${elapsed.inSeconds % 60} minute(s).');
+    return LocationTrackResponse(location, null);
+  }
+}
+
+class LocationTrackResponse {
+  final Location location;
+  final Weather weather;
+  LocationTrackResponse(this.location, this.weather);
 }
